44c49edd80d1333c32b9b418c3e776300124fd88
Akshit Beniwal <akshitbeniwal26@gmail.com>
2024-09-10 16:40:24 +1000

    Add script for running FastSurfer preprocessing
    This commit adds a new script, srun_fastsurfer_user.sh, which is used for running the preprocessing step of FastSurfer. The script sets up the necessary SLURM job parameters and defines the paths for input data, output files, and the FastSurfer image. It also sets the temporary directory and executes the FastSurfer preprocessing command using Singularity.
    
    The script is currently commented out, as the Singularity image and license arguments are not provided. The commented out command can be uncommented and modified with the appropriate paths and arguments.
    
    Note: The script does not have a newline at the end of the file.

---
+++ pre_processing/srun_fastsurfer_user.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --job-name=fastsurfer_job1 
+#SBATCH --ntasks=1 
+#SBATCH --partition=p100                 
+#SBATCH --gres=gpu:1                                    
+#SBATCH --time=24:00:00 
+
+# Set paths based on your arguments
+outputdir="$1"
+datadir="$2"
+# fastsurfer_image="$3"  # Use the provided argument
+sid="$3"
+# license="$5"
+
+data_dir="/home/groups/dlmrimnd/akshit/data:/data" 
+output_dir="/home/groups/dlmrimnd/akshit/output_files:/MND_output_files"
+FS_dir="/home/groups/dlmrimnd/akshit/freesurfer_license:/freesurfer_license"
+# Load the Singularity module (if needed)
+
+
+# Singularity execution command
+# singularity run --nv --no-home \
+#   -B "$data_dir" \
+#   -B "$output_dir" \
+#   -B "$FS_dir" \
+#   "$fastsurfer_image" \
+#   --fs_license "$license" \
+#   --sd "$outputdir" \
+#   --t1 "$datadir" \
+#   --sid "$sid"
+export TMPDIR="/home/groups/dlmrimnd/akshit/temp"
+singularity exec --nv --no-home \
+                /home/groups/dlmrimnd/akshit/fastsurfer-latest.sif \
+                ./run_fastsurfer.sh \
+                --fs_license /home/groups/dlmrimnd/akshit/freesurfer_license/license.txt \
+                --t1 "$datadir" \
+                --sid "$sid" \
+                --sd "$outputdir" \
+                --py python --fsqsphere
